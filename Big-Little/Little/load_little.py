dd7fb43fc1d3edc16409a96cdac33c56c2598139
moyarzun <mauricio2141@gmail.com>
2017-12-18 21:44:04 -0300

    Little clasifica 0 y 1

--- Big-Little/Little/load_little.py
+++ Big-Little/Little/load_little.py
@@ -1,103 +1,148 @@
 import numpy as np
 from keras.datasets import mnist
+from keras.models import Sequential
 from keras.models import load_model
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers import Activation
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
 from keras.utils import np_utils
-from datetime import datetime
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
-import zmq, sys, pickle, argparse, copy, math
 
-filenames = '01_u'
-port = '5000'
-port_end = '5001'
-
-# Fijar semilla para reproducir experimento
+# Fixed seed for experiment replayability
 seed = 2141
 np.random.seed(seed)
 
-# Descargar dataset
+filenames = 'little'
+n_classes = 3
+
+# Dataset download (If not local, from Internet)
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-## Gets IP and PORT from command line and parses them
-ConnectionInfo = argparse.ArgumentParser()
-ConnectionInfo.add_argument("-i",  default='127.0.0.1')
-ConnectionInfo.add_argument("-o",  default='0.0.0.0')
-ConnectionInfo.add_argument("-c",  default=math.floor(np.random.random() * 1000))
-ConnectionInfoParsed = ConnectionInfo.parse_args()
+###############################
+# Re-tagging the dataset for the specific classes we want to train
+# 0 -> Class 1
+# 1 -> Class 2
+# 2 -> 'Others'
+y_train[y_train==0]=0
+y_train[y_train==1]=1
+y_train[y_train>=2]=2
 
-# Saves the parsed IP and Port
-ip_in = ConnectionInfoParsed.i
-ip_out = ConnectionInfoParsed.o
-sample = int(ConnectionInfoParsed.c)
+y_test[y_test==0]=0
+y_test[y_test==1]=1
+y_test[y_test>=2]=2
+###############################
 
-print("MNIST Sample", int(sample))
-foo = X_test[sample]
-print("Expected class: ", y_test[sample])
+# Building the input vector from the 28x28 pixels
+X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
 
-#Inicia medición de tiempo
-start = datetime.now()
+# Normalizing the data
+X_train /= 255
+X_test /= 255
 
-print('Image preprocessing...')
-foo = np.expand_dims(foo, axis=0)
-message = foo
+# One-hot encoding
+Y_train = np_utils.to_categorical(y_train, n_classes)
+Y_test = np_utils.to_categorical(y_test, n_classes)
 
-# building the input vector from the 28x28 pixels
-foo = foo.reshape(foo.shape[0], 1, 28, 28).astype('float32')
-#X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+# Model design
+def baseline_model():
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu', data_format='channels_first'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(n_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
-# normalizing the data to help with the training
-foo /= 255
+# Model building
+model = baseline_model()
 
-print('Preprocessing done.')
-print('Loading model and tensors...')
+# Model compiling
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-# Cargar modelo preguardado
-model = load_model(filenames + '_model.h5')
-model.load_weights(filenames + '_tensors.h5')
+# Model training and metrics capture
+history = model.fit(X_train, Y_train,
+          batch_size=200, epochs=5,
+          verbose=1,
+          validation_data=(X_test, Y_test))
 
-print('Model loading done.')
-print('Classifying...')
+# Saving model
+model.save(filenames + '_model.h5')
+# Saving weights
+model.save_weights(filenames + '_tensors.h5')
+print('Model saved.')
+
+# Plotting the metrics
+fig = plt.figure()
+plt.subplot(2,1,1)
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='lower right')
+
+plt.subplot(2,1,2)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper right')
+
+plt.tight_layout()
+
+plt.savefig(filenames + '_build_met.png')
+print("Metrics saved as " + filenames + "'_build_met.png'.")
+
+loss_and_metrics = model.evaluate(X_test, Y_test, verbose=2)
+
+print("Test Loss", loss_and_metrics[0])
+print("Test Accuracy", loss_and_metrics[1])
 
 # load the model and create predictions on the test set
-class_start = datetime.now()
-predicted_classes = model.predict_classes(foo)
-class_end = datetime.now() - class_start
+predicted_classes = model.predict_classes(X_test)
 
-print('---------------------------')
-print("Value predicted: ", predicted_classes)
-print('Classification done in (hh:mm:ss.ms) {}'.format(class_end))
-if predicted_classes == 2:
-    print("Predicted class: 'other'...")
-    print("Continuing classification at next node...")
-    # ZeroMQ Context
-    context = zmq.Context()
-    # Preparing ZeroMQ context for the next node...
-    sock = context.socket(zmq.REQ)
-    sock.bind('tcp://0.0.0.0:'+port)
-    sock.send(pickle.dumps(message))
-    X_answer = sock.recv()
-    personal = datetime.now() - start
-    print('Node processing time (hh:mm:ss.ms) {}'.format(personal))
-    print('Data sent. Waiting for classification...')
-    sock.close()
+# See which we predicted correctly and which not
+correct_indices = np.nonzero(predicted_classes == y_test)[0]
+incorrect_indices = np.nonzero(predicted_classes != y_test)[0]
+print()
+print(len(correct_indices)," classified correctly")
+print(len(incorrect_indices)," classified incorrectly")
 
-    # Espera hasta que concluya la clasificación
-    sock = context.socket(zmq.REQ)
-    sock.bind('tcp://0.0.0.0:'+port_end)
-    for x in range (1, 6):
-        sock.send_string('ack')
-        result = sock.recv()
-        result = pickle.loads(result)
-        if result == -1:
-            print("Node "+ str(x) +" couldn't classify your sample.")
-        else:
-            print("Node "+ str(x) +" predicted class: ", result)
-            break
-    sock.close()
-else:
-    print("Predicted class: ", predicted_classes)
+# Adapt figure size to accomodate 18 subplots
+plt.rcParams['figure.figsize'] = (7,14)
 
-total = datetime.now() - start
-print('Network processing time (hh:mm:ss.ms) {}'.format(total))
-print('Done!')
+figure_evaluation = plt.figure()
+
+# Plot 9 correct predictions
+for i, correct in enumerate(correct_indices[:9]):
+    plt.subplot(6,3,i+1)
+    plt.imshow(X_test[correct].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title(
+      "Predicted: {}, Truth: {}".format(predicted_classes[correct],
+                                        y_test[correct]))
+    plt.xticks([])
+    plt.yticks([])
+
+# Plot 9 incorrect predictions
+for i, incorrect in enumerate(incorrect_indices[:9]):
+    plt.subplot(6,3,i+10)
+    plt.imshow(X_test[incorrect].reshape(28,28), cmap='gray', interpolation='none')
+    plt.title(
+      "Predicted {}, Truth: {}".format(predicted_classes[incorrect],
+                                       y_test[incorrect]))
+    plt.xticks([])
+    plt.yticks([])
+
+figure_evaluation
+
+plt.savefig(filenames + '_build_pred.png')
+print("Predictions saved as '" + filenames + "_build_pred.png'.")
